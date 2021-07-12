ef441e07c550c3011d398afc365583ba446c5193
= <samgusaapp1993@gmail.com>
2021-07-12 18:08:05 -0400

    Initial Commit

---
+++ WeSplit_Test/WeSplit_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_TestApp.swift
+//  WeSplit_Test
+//
+//  Created by Sam Greenhill on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
