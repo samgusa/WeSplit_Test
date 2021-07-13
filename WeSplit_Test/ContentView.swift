f4b9defa645982c9cea559109942303d79d9ca72
= <samgusaapp1993@gmail.com>
2021-07-12 23:07:28 -0400

    Just setting up the view.

--- WeSplit_Test/ContentView.swift
+++ WeSplit_Test/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State automatically checks for changes.
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }
+        }
     }
 }
 
